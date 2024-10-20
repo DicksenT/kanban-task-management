801212acca4d2aabcf9ad6706806cf81759cc467
Dicksen Tan <tandicksen@gmail.com>
2024-10-20 09:41:24 +0700

    Add: Auth Context provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import './index.css'
 
 import { KanbanContextProvider } from './context/KanbanContext.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <KanbanContextProvider>
-    <App />
-    </KanbanContextProvider>
+    <AuthContextProvider>
+      <KanbanContextProvider>
+        <App />
+      </KanbanContextProvider>
+    </AuthContextProvider>
 
   </React.StrictMode>,
 )
